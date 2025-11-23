e6784bfc6fcc1a94588473b66504a35905732bb6
hamedi786 <154887900+hamedi786@users.noreply.github.com>
2025-11-23 16:28:48 +0430

    make member table

---
+++ main.sql
@@ -0,0 +1,9 @@
+create table member (
+    m_id number primary key,
+    f_name varchar2(50),
+    fa_name varchar2(50),
+    dob date,
+    phone varchar2(15),
+    join_date date,
+    status varchar2(20)
+);
