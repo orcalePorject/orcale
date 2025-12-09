e57e11f1d76268eb5f22db3e820e63e1bfa220c1
MR-mohammadi-sab <asf423796@gmail.com>
2025-12-09 01:10:48 -0800

    a little cleacn the code

--- data.sql
+++ data.sql
@@ -42,3 +42,233 @@ INSERT INTO class_bookings (class_id, member_id, booking_date, start_time, end_t
 (9, 19, DATE '2025-12-09', TIMESTAMP '2025-12-09 17:30:00', TIMESTAMP '2025-12-09 18:30:00', 'BOOKED');
 INSERT INTO class_bookings (class_id, member_id, booking_date, start_time, end_time, status) VALUES
 (10, 20, DATE '2025-12-09', TIMESTAMP '2025-12-09 15:00:00', TIMESTAMP '2025-12-09 16:00:00', 'BOOKED');
+
+-- member
+INSERT INTO member (f_name, l_name, dob, phone, email, address, created_by)
+VALUES
+('Rahim','Sultani', DATE '1995-02-12','0700123456','rahims@gmail.com','Kabul, Karte 3',1);
+INSERT INTO member (f_name, l_name, dob, phone, email, address, created_by)
+VALUES
+('Maryam','Naseeri',DATE '1999-11-20','0799456123','maryamn@gmail.com','Kabul, Shahr-e-Naw',2);
+INSERT INTO member (f_name, l_name, dob, phone, email, address, created_by)
+VALUES
+('Jawad','Ahmadi',DATE '1990-03-05','0789001122','jawada@gmail.com','Kabul, Makrorayan 2',1);
+
+INSERT INTO member (f_name, l_name, dob, phone, email, address, created_by)
+VALUES
+('Hamid','Rashidi',DATE '1988-09-11','0700654321','hamidr@gmail.com','Mazar, Karte Ariana',2);
+INSERT INTO member (f_name, l_name, dob, phone, email, address, created_by)
+VALUES
+('Sajjad','Fahimi',DATE '1997-12-22','0799876543','sajjadf@gmail.com','Kabul, Kart-e-Parwan',1);
+
+INSERT INTO member (f_name, l_name, dob, phone, email, address, created_by)
+VALUES
+('Omid','Sangin',DATE '1996-10-01','0799001122','omids@gmail.com','Kabul, Darulaman',2);
+INSERT INTO member (f_name, l_name, dob, phone, email, address, created_by)
+VALUES
+('Haseeb','Qaderi',DATE '1994-07-14','0700554433','haseebq@gmail.com','Herat, Injil',2);
+INSERT INTO member (f_name, l_name, dob, phone, email, address, created_by)
+VALUES
+('Neda','Karim',DATE '2002-01-25','0789012344','nedakarim@gmail.com','Mazar, Dehdadi',1);
+
+
+-- membership 
+INSERT INTO membership (plan_code, plan_desc, duration_days, price, is_active) VALUES
+('BASIC_MONTH', 'Basic monthly membership', 30, 800, 'y');
+INSERT INTO membership (plan_code, plan_desc, duration_days, price, is_active) VALUES
+('FULL_MONTH', 'Full access monthly membership', 30, 1200, 'y');
+INSERT INTO membership (plan_code, plan_desc, duration_days, price, is_active) VALUES
+('QUARTER_BASIC', 'Quarterly basic membership', 90, 2100, 'y');
+INSERT INTO membership (plan_code, plan_desc, duration_days, price, is_active) VALUES
+('HALF_YEAR', 'Half-year full membership', 180, 6000, 'y');
+INSERT INTO membership (plan_code, plan_desc, duration_days, price, is_active) VALUES
+('ANNUAL_BASIC', 'Annual basic plan', 365, 11000, 'y');
+INSERT INTO membership (plan_code, plan_desc, duration_days, price, is_active) VALUES
+('VIP_MONTH', 'VIP monthly membership with all facilities', 30, 2000, 'y');
+INSERT INTO membership (plan_code, plan_desc, duration_days, price, is_active) VALUES
+('STUDENT_MONTH', 'Discounted monthly plan for students', 30, 600, 'y');
+INSERT INTO membership (plan_code, plan_desc, duration_days, price, is_active) VALUES
+('WEEKEND_PLAN', 'Access on weekends only', 30, 500, 'y');
+INSERT INTO membership (plan_code, plan_desc, duration_days, price, is_active) VALUES
+('YOGA_PLAN', 'Yoga and wellness monthly', 30, 700, 'y');
+INSERT INTO membership (plan_code, plan_desc, duration_days, price, is_active) VALUES
+('CROSSFIT_PLAN', 'CrossFit training monthly', 30, 900, 'y');
+
+-- member_subscriptions
+INSERT INTO member_subscriptions ( m_id, plan_code, start_date, end_date) VALUES
+(13, 'BASIC_MONTH', DATE '2025-12-01', DATE '2025-12-31');
+INSERT INTO member_subscriptions ( m_id, plan_code, start_date, end_date) VALUES
+(14, 'QUARTER_BASIC', DATE '2025-10-01', DATE '2025-12-31');
+INSERT INTO member_subscriptions ( m_id, plan_code, start_date, end_date) VALUES
+(15, 'HALF_YEAR', DATE '2025-07-01', DATE '2025-12-31');
+INSERT INTO member_subscriptions ( m_id, plan_code, start_date, end_date) VALUES
+(16, 'ANNUAL_BASIC', DATE '2025-01-01', DATE '2025-12-31');
+INSERT INTO member_subscriptions ( m_id, plan_code, start_date, end_date) VALUES
+(17, 'VIP_MONTH', DATE '2025-12-01', DATE '2025-12-31');
+INSERT INTO member_subscriptions ( m_id, plan_code, start_date, end_date) VALUES
+(18, 'STUDENT_MONTH', DATE '2025-12-01', DATE '2025-12-31');
+INSERT INTO member_subscriptions ( m_id, plan_code, start_date, end_date) VALUES
+(19, 'WEEKEND_PLAN', DATE '2025-12-01', DATE '2025-12-31');
+INSERT INTO member_subscriptions ( m_id, plan_code, start_date, end_date) VALUES
+(20, 'YOGA_PLAN', DATE '2025-12-01', DATE '2025-12-31');
+
+
+-- Insert staff
+INSERT INTO staff ( first_name,last_name, phone, email, role, username, password_hash, salary) VALUES
+('Ahmad','Hamedi', '0799433222', 'admin@email.com', 'ADMIN', 'admin', 'admin123', 50000);
+
+INSERT INTO staff ( first_name,last_name, phone, email, role, username, password_hash, salary) VALUES
+( 'Asif','Mohammadi', '0791781674', 'reception@email.com', 'RECEPTION', 'reception', 'recep123', 30000);
+
+INSERT INTO staff ( first_name,last_name, phone, email, role, username, password_hash, salary) VALUES
+( 'Ali','Azizi', '0791733674', null, 'CLEANER', null, null, 3000);
+
+INSERT INTO staff (first_name, last_name, phone, email, role, username, password_hash, salary)
+VALUES 
+('Reza','Qasemi','0799010101','rqasemi@gmail.com','ADMIN','reza_admin','rez123',51000);
+INSERT INTO staff (first_name, last_name, phone, email, role, username, password_hash, salary)
+VALUES 
+('Yama','Payman','0799009900','yamapayman@gmail.com','RECEPTION','yama_rec','yama123',26000);
+INSERT INTO staff (first_name, last_name, phone, email, role, username, password_hash, salary)
+VALUES 
+('Esmat','Latifi','0799008800','esmatlatifi@gmail.com','CLEANER',NULL,NULL,7500);
+INSERT INTO staff (first_name, last_name, phone, email, role, username, password_hash, salary)
+VALUES 
+('Shabnam','Karimi','0799007700','shabnamkarimi@gmail.com','MANAGER','sh_kari','shab123',47000);
+INSERT INTO staff (first_name, last_name, phone, email, role, username, password_hash, salary)
+VALUES 
+('Farhad','Nouri','0799006600','farhadnoori@gmail.com','ADMIN','farhad_admin','far123',48000);
+INSERT INTO staff (first_name, last_name, phone, email, role, username, password_hash, salary)
+VALUES 
+('Bilal','Hosseini','0799005500','bilah@gmail.com','CLEANER',NULL,NULL,8000);
+INSERT INTO staff (first_name, last_name, phone, email, role, username, password_hash, salary)
+VALUES 
+('Sahar','Rahimi','0799004400','sahar.@gmail.com','RECEPTION','sahar_rec','sahar123',28000);
+INSERT INTO staff (first_name, last_name, phone, email, role, username, password_hash, salary)
+VALUES 
+('Fawad','Sediqi','0799003300','fsediqi@yahoo.com','MANAGER','fawad_mgr','mgr123',45000);
+
+-- trainers
+
+INSERT INTO trainers (first_name, last_name, phone, email, specialization, hourly_rate)
+VALUES
+('Murtaza','Karimi','0799550011','m.karimi@gmail.com','Bodybuilding',500);
+INSERT INTO trainers (first_name, last_name, phone, email, specialization, hourly_rate)
+VALUES
+('Tamana','Rahmani','0799550022','t.rahmani@gmail.com','Yoga',400);
+INSERT INTO trainers (first_name, last_name, phone, email, specialization, hourly_rate)
+VALUES
+('Farid','Latifi','0799550033','farid.latifi@yahoo.com','CrossFit',550);
+INSERT INTO trainers (first_name, last_name, phone, email, specialization, hourly_rate)
+VALUES
+('Shakila','Nawabi','0799550044','sh.nawabi@gmail.com','Zumba',450);
+INSERT INTO trainers (first_name, last_name, phone, email, specialization, hourly_rate)
+VALUES
+('Haroon','Qaderi','0799550055','h.qaderi@gmail.com','Strength Training',600);
+INSERT INTO trainers (first_name, last_name, phone, email, specialization, hourly_rate)
+VALUES
+('Wahid','Sakhi','0799550066','wahid.sakhi@gmail.com','Cardio',350);
+INSERT INTO trainers (first_name, last_name, phone, email, specialization, hourly_rate)
+VALUES
+('Nargis','Faizi','0799550077','n.faizi@gmail.com','Pilates',480);
+INSERT INTO trainers (first_name, last_name, phone, email, specialization, hourly_rate)
+VALUES
+('Yasin','Ahmadzai','0799550088','yasin.ah@gmail.com','Kickboxing',550);
+INSERT INTO trainers (first_name, last_name, phone, email, specialization, hourly_rate)
+VALUES
+('Bilquis','Hashimi','0799550099','b.hashimi@gmail.com','Aerobics',400);
+INSERT INTO trainers (first_name, last_name, phone, email, specialization, hourly_rate)
+VALUES
+('Sami','Rohani','0799550100','s.rohani@gmail.com','Martial Arts',650);
+
+-- payments
+
+INSERT INTO payments (member_id, amount, description, received_by)
+VALUES
+(13,800,'Monthly Basic Fee',2);
+INSERT INTO payments (member_id, amount, description, received_by)
+VALUES
+(14,1200,'Monthly Full Fee',1);
+INSERT INTO payments (member_id, amount, description, received_by)
+VALUES
+(15,2100,'Quarter Basic Payment',1);
+INSERT INTO payments (member_id, amount, description, received_by)
+VALUES
+(16,800,'Monthly Renewal',2);
+INSERT INTO payments (member_id, amount, description, received_by)
+VALUES
+(17,6000,'Half Year Payment',1);
+INSERT INTO payments (member_id, amount, description, received_by)
+VALUES
+(18,2000,'VIP Monthly',15);
+INSERT INTO payments (member_id, amount, description, received_by)
+VALUES
+(19,11000,'Annual Basic Plan',1);
+INSERT INTO payments (member_id, amount, description, received_by)
+VALUES
+(20,1200,'Monthly Full Access',2);
+
+-- lockers
+INSERT INTO lockers (locker_number, member_id, status, key_number, start_date, end_date, deposit)
+VALUES
+('L001',19,'OCCUPIED','K001',DATE'2025-01-01',DATE'2025-02-01',200);
+INSERT INTO lockers (locker_number, member_id, status, key_number, start_date, end_date, deposit)
+VALUES
+('L002',18,'OCCUPIED','K002',DATE'2025-01-05',DATE'2025-02-05',200);
+INSERT INTO lockers (locker_number, member_id, status, key_number, start_date, end_date, deposit)
+VALUES
+('L003',NULL,'AVAILABLE',NULL,NULL,NULL,0);
+INSERT INTO lockers (locker_number, member_id, status, key_number, start_date, end_date, deposit)
+VALUES
+('L004',13,'OCCUPIED','K004',DATE'2025-01-10',DATE'2025-02-10',200);
+INSERT INTO lockers (locker_number, member_id, status, key_number, start_date, end_date, deposit)
+VALUES
+('L005',14,'OCCUPIED','K005',DATE'2025-01-12',DATE'2025-02-12',200);
+INSERT INTO lockers (locker_number, member_id, status, key_number, start_date, end_date, deposit)
+VALUES
+('L006',NULL,'AVAILABLE',NULL,NULL,NULL,0);
+INSERT INTO lockers (locker_number, member_id, status, key_number, start_date, end_date, deposit)
+VALUES
+('L007',15,'OCCUPIED','K007',DATE'2025-01-20',DATE'2025-02-20',200);
+INSERT INTO lockers (locker_number, member_id, status, key_number, start_date, end_date, deposit)
+VALUES
+('L008',16,'OCCUPIED','K008',DATE'2025-01-15',DATE'2025-02-15',200);
+INSERT INTO lockers (locker_number, member_id, status, key_number, start_date, end_date, deposit)
+VALUES
+('L009',NULL,'AVAILABLE',NULL,NULL,NULL,0);
+INSERT INTO lockers (locker_number, member_id, status, key_number, start_date, end_date, deposit)
+VALUES
+('L010',17,'OCCUPIED','K010',DATE'2025-01-18',DATE'2025-02-18',200);
+
+-- equipment
+
+INSERT INTO equipment (equipment_name, category, purchase_date, price, status, location)
+VALUES
+('Treadmill A', 'CARDIO', DATE'2023-05-01', 45000, 'AVAILABLE','Hall A');
+INSERT INTO equipment (equipment_name, category, purchase_date, price, status, location)
+VALUES
+('Treadmill B', 'CARDIO', DATE'2023-06-11', 46000, 'AVAILABLE','Hall A');
+INSERT INTO equipment (equipment_name, category, purchase_date, price, status, location)
+VALUES
+('Bench Press', 'STRENGTH', DATE'2022-11-15', 25000, 'IN_USE','Gym Floor');
+INSERT INTO equipment (equipment_name, category, purchase_date, price, status, location)
+VALUES
+('Squat Rack', 'STRENGTH', DATE'2023-01-20', 30000, 'AVAILABLE','Gym Floor');
+INSERT INTO equipment (equipment_name, category, purchase_date, price, status, location)
+VALUES
+('Dumbbell Set 1', 'FREE_WEIGHTS', DATE'2023-02-18', 15000, 'AVAILABLE','Weights Area');
+INSERT INTO equipment (equipment_name, category, purchase_date, price, status, location)
+VALUES
+('Dumbbell Set 2', 'FREE_WEIGHTS', DATE'2023-03-10', 16000,'AVAILABLE','Weights Area');
+INSERT INTO equipment (equipment_name, category, purchase_date, price, status, location)
+VALUES
+('Elliptical Machine', 'CARDIO', DATE'2023-04-14', 32000,'MAINTENANCE','Workshop');
+INSERT INTO equipment (equipment_name, category, purchase_date, price, status, location)
+VALUES
+('Leg Press Machine', 'STRENGTH', DATE'2022-10-22', 38000,'IN_USE','Gym Floor');
+INSERT INTO equipment (equipment_name, category, purchase_date, price, status, location)
+VALUES
+('Rowing Machine', 'CARDIO', DATE'2023-01-30', 35000,'AVAILABLE','Hall B');
+INSERT INTO equipment (equipment_name, category, purchase_date, price, status, location)
+VALUES
+('Pull-up Bar', 'STRENGTH', DATE'2022-12-12', 8000,'AVAILABLE','Gym Floor');
---
+++ tables.sql
@@ -0,0 +1,200 @@
+
+create table member (
+    m_id number GENERATED ALWAYS as IDENTITY primary key,
+    f_name varchar2(50) not null,
+    l_name varchar2(50),
+    dob date,
+    phone varchar2(15) not null,
+    email varchar2(100),
+    address VARCHAR2(150),
+    join_date date default sysdate not null,
+    status varchar2(20) default 'ACTIVE' NOT NULL,
+    created_at TIMESTAMP default CURRENT_timeStamp,
+
+    CONSTRAINT chk_states CHECK (status in ('ACTIVE','INACTIVE','SUSPENDED','EXPIRED'))
+);
+alter table member add created_by NUMBER;
+alter table member add CONSTRAINT sttaf_in_mem_fk FOREIGN key(created_by) REFERENCES staff(staff_id);
+
+select * from member;
+
+-- membership_plans
+create TABLE membership (
+    plan_id NUMBER GENERATED ALWAYS as IDENTITY primary key,
+    plan_code VARCHAR2(20) unique not NULL,
+    plan_desc VARCHAR2(200),
+    duration_days NUMBER not NULL,
+    price NUMBER not null ,
+    is_active CHAR(1) default 'y' not null ,
+    CONSTRAINT check_is_active CHECK (is_active in ('y','n'))
+)
+
+-- member_subscription
+create table member_subscriptions (
+    sub_id NUMBER GENERATED ALWAYS as IDENTITY primary key,
+    m_id NUMBER ,
+    plan_code VARCHAR2(20) ,
+    start_date DATE not NULL,
+    end_date DATE not NULL,
+    CONSTRAINT fk_membership_in_member_subscriptions foreign key(plan_code) references membership(plan_code),
+    CONSTRAINT fk_member_in_member_subscriptions foreign key(m_id) references member(m_id)
+)
+
+
+-- sequence for staff
+CREATE SEQUENCE seq_staff_id START WITH 1 INCREMENT BY 1;
+
+--  staff table
+CREATE TABLE staff (
+    staff_id      NUMBER DEFAULT seq_staff_id.NEXTVAL CONSTRAINT staf_pk PRIMARY KEY,
+    first_name     VARCHAR2(100) NOT NULL,
+    last_name     VARCHAR2(100) NOT NULL,
+    phone         VARCHAR2(15) NOT NULL,
+    email         VARCHAR2(100),
+    role      VARCHAR2(50) NOT NULL CONSTRAINT staff_chk_role CHECK (role IN ('ADMIN', 'RECEPTION', 'CLEANER', 'MANAGER')),
+    username      VARCHAR2(50) UNIQUE,
+    password_hash VARCHAR2(200) ,
+    hire_date     DATE DEFAULT SYSDATE,
+    salary        NUMBER(10,2),
+    status        VARCHAR2(20) DEFAULT 'ACTIVE' CHECK (status IN ('ACTIVE', 'INACTIVE', 'ON_LEAVE')),
+    created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE staff
+MODIFY  username VARCHAR2(100);
+ALTER TABLE staff
+MODIFY  password_hash VARCHAR2(100);
+
+select * from staff;
+
+
+CREATE SEQUENCE seq_trainer_id START WITH 1 INCREMENT BY 1;
+--  trainers
+CREATE TABLE trainers (
+    trainer_id     NUMBER DEFAULT seq_trainer_id.NEXTVAL PRIMARY KEY,
+    first_name      VARCHAR2(100) NOT NULL,
+    last_name      VARCHAR2(100) NOT NULL,
+    phone          VARCHAR2(15) NOT NULL,
+    email          VARCHAR2(100),
+    specialization VARCHAR2(200),
+    hire_date      DATE DEFAULT SYSDATE,
+    hourly_rate    NUMBER(10,2),
+    status         VARCHAR2(20) DEFAULT 'ACTIVE' CHECK (status IN ('ACTIVE', 'INACTIVE'))
+);
+
+select * from trainers;
+-- create attendance tables 
+
+CREATE TABLE member_attendance (
+    att_date    DATE DEFAULT TRUNC(SYSDATE),
+    member_id   NUMBER NOT NULL,
+    is_present  NUMBER(1) DEFAULT 0,
+    CONSTRAINT mem_in_att_fk FOREIGN key(member_id) REFERENCES member(m_id),
+     CONSTRAINT m_att_chk check (is_present in (0,1)),
+   CONSTRAINT m_att_pk PRIMARY KEY (att_date, member_id)
+);
+
+
+CREATE TABLE staff_attendance (
+    att_date    DATE DEFAULT TRUNC(SYSDATE),
+    staff_id    NUMBER NOT NULL,
+    is_present  NUMBER(1) DEFAULT 0,
+
+    CONSTRAINT sttaf_in_att_fk FOREIGN key(staff_id) REFERENCES staff(staff_id),
+    CONSTRAINT s_att_chk check (is_present in (0,1)),
+    CONSTRAINT s_att_pk PRIMARY KEY (att_date, staff_id)
+);
+
+
+CREATE TABLE trainer_attendance (
+    att_date    DATE DEFAULT TRUNC(SYSDATE),
+    trainer_id  NUMBER NOT NULL,
+    is_present  NUMBER(1) DEFAULT 0,
+    
+    CONSTRAINT t_in_att_fk FOREIGN key(trainer_id) REFERENCES trainers(trainer_id),
+    CONSTRAINT t_att_chk check (is_present in (0,1)),
+    CONSTRAINT t_att_pk PRIMARY KEY (att_date, trainer_id)
+);
+
+CREATE SEQUENCE seq_payment_id START WITH 1 INCREMENT BY 1;
+--  payments
+CREATE TABLE payments (
+    payment_id   NUMBER DEFAULT seq_payment_id.NEXTVAL PRIMARY KEY,
+    member_id    NUMBER,
+    amount       NUMBER(10,2) NOT NULL,
+    payment_date DATE DEFAULT SYSDATE,
+    description  VARCHAR2(200),
+    received_by  NUMBER,
+    status       char(1) DEFAULT 'y',
+    CONSTRAINT chk_in_payments CHECK (status IN ('y','n')),
+    CONSTRAINT mem_in_payment_fk FOREIGN key(member_id) REFERENCES member(m_id),
+    CONSTRAINT sttaf_in_payments_fk FOREIGN key(received_by) REFERENCES staff(staff_id)
+);
+
+select * from payments;
+
+CREATE SEQUENCE seq_class_id START WITH 1 INCREMENT BY 1;
+
+-- classes
+CREATE TABLE classes (
+    class_id      NUMBER DEFAULT seq_class_id.NEXTVAL PRIMARY KEY,
+    class_name    VARCHAR2(100) NOT NULL,
+    trainer_id    NUMBER,
+    description   VARCHAR2(200),
+    max_capacity  NUMBER DEFAULT 20,
+    duration      NUMBER DEFAULT 60, -- minutes
+    is_active     CHAR(1) DEFAULT 'Y',
+    CONSTRAINT chk_in_classes CHECK (is_active IN ('Y', 'N')),
+    CONSTRAINT t_in_classes_fk FOREIGN key(trainer_id) REFERENCES trainers(trainer_id)
+);
+
+CREATE SEQUENCE seq_booking_id START WITH 1 INCREMENT BY 1;
+-- class_bookings
+CREATE TABLE class_bookings (
+    class_id     NUMBER,
+    member_id    NUMBER,
+    booking_date DATE NOT NULL,
+    start_time   TIMESTAMP NOT NULL,
+    end_time     TIMESTAMP NOT NULL,
+    status       VARCHAR2(20) DEFAULT 'BOOKED', 
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_inbooking_class primary key (class_id,member_id,booking_date),
+    CONSTRAINT chk_in_class_booking CHECK (status IN ('BOOKED', 'ATTENDED', 'CANCELLED', 'NO_SHOW'))
+);
+
+
+CREATE SEQUENCE seq_equipment_id START WITH 1 INCREMENT BY 1;
+--  equipment
+CREATE TABLE equipment (
+    equipment_id   NUMBER DEFAULT seq_equipment_id.NEXTVAL PRIMARY KEY,
+    equipment_name VARCHAR2(100) NOT NULL,
+    category       VARCHAR2(50) ,
+    CONSTRAINT chk_in_equipment_category CHECK (category IN ('CARDIO', 'STRENGTH', 'FREE_WEIGHTS')),
+    purchase_date  DATE,
+    price          NUMBER(10,2),
+    status         VARCHAR2(20) DEFAULT 'AVAILABLE',
+    CONSTRAINT chk_in_equipment_status CHECK (status IN ('AVAILABLE', 'IN_USE', 'MAINTENANCE')),
+    location       VARCHAR2(50),
+    last_check     DATE
+);
+
+CREATE SEQUENCE seq_locker_id START WITH 1 INCREMENT BY 1;
+
+--  lockers
+CREATE TABLE lockers (
+    locker_id     NUMBER DEFAULT seq_locker_id.NEXTVAL PRIMARY KEY,
+    locker_number VARCHAR2(10) UNIQUE NOT NULL,
+    member_id     NUMBER ,
+    status        VARCHAR2(20) DEFAULT 'AVAILABLE',
+    CONSTRAINT CHK_IN_LOCKER_STATUS CHECK (status IN ('AVAILABLE', 'OCCUPIED', 'MAINTENANCE','RESERVED')),
+    key_number    VARCHAR2(20),
+    start_date    DATE,
+    end_date      DATE,
+    deposit       NUMBER(10,2) DEFAULT 0,
+     CONSTRAINT mem_in_locker_fk FOREIGN key(member_id) REFERENCES member(m_id)
+    
+);
+
+
+select * from lockers;
+
