3ef003eebe112cc18884e0bbfe5d8a48676461b0
MR-mohammadi-sab <asf423796@gmail.com>
2025-12-09 05:52:45 -0800

    create procedure for register member

---
+++ procedures.sql
@@ -0,0 +1,42 @@
+-- register member
+
+CREATE OR REPLACE PROCEDURE sp_register_member(
+    p_f_name      VARCHAR2,
+    p_l_name      VARCHAR2,
+    p_dob         DATE,
+    p_phone       VARCHAR2,
+    p_email       VARCHAR2,
+    p_address     VARCHAR2,
+    p_plan_code   VARCHAR2,
+    p_created_by  NUMBER
+) IS
+    v_member_id NUMBER;
+    v_duration_days NUMBER;
+BEGIN
+    -- وارد کردن اطلاعات عضو
+    INSERT INTO member (f_name, l_name, dob, phone, email, address, created_by)
+    VALUES (p_f_name, p_l_name, p_dob, p_phone, p_email, p_address, p_created_by)
+    RETURNING m_id INTO v_member_id;
+    
+    -- ایجاد اشتراک
+    SELECT duration_days INTO v_duration_days
+    FROM membership WHERE plan_code = p_plan_code;
+    
+    INSERT INTO member_subscriptions (m_id, plan_code, start_date, end_date)
+    VALUES (v_member_id, p_plan_code, SYSDATE, SYSDATE + v_duration_days);
+    
+    COMMIT;
+    
+    DBMS_OUTPUT.PUT_LINE('Member registered successfully. ID: ' || v_member_id);
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+        RAISE;
+END;
+/
+
+BEGIN
+ SP_REGISTER_MEMBER('Maner','Ahmadi',Date '2004-09-12','0799776554','maner@gmail.com','Mazer sharif,3 street','VIP_MONTH',15);
+ END;
+/
