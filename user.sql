48880910daf8f72188786a2c758bee7d2c887bdb
hamedi786 <154887900+hamedi786@users.noreply.github.com>
2025-12-11 16:09:16 +0430

    give privilage to gym_reception  user

--- user.sql
+++ user.sql
@@ -1,2 +1,8 @@
 CREATE USER gym_reception IDENTIFIED BY reception123;
 CREATE USER gym_manager IDENTIFIED BY manager123;
+
+GRANT SELECT, INSERT, UPDATE ON member TO gym_reception;
+GRANT SELECT, INSERT ON payments TO gym_reception;
+GRANT SELECT ON membership TO gym_reception;
+GRANT SELECT ON v_active_members TO gym_reception;
+GRANT EXECUTE ON pkg_gym_management TO gym_reception;
