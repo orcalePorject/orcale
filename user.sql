405908bdc393785ebe75981232b86d7d35c9c552
MR-mohammadi-sab <asf423796@gmail.com>
2025-12-11 04:14:33 -0800

    add some changes

--- user.sql
+++ user.sql
@@ -2,10 +2,16 @@ CREATE USER gym_reception IDENTIFIED BY reception123;
 CREATE USER gym_manager IDENTIFIED BY manager123;
 CREATE USER admin IDENTIFIED BY admin123;
 
-GRANT CREATE SESSION TO gym_admin, gym_reception, gym_manager;
-GRANT UNLIMITED TABLESPACE TO gym_admin;
-GRANT SELECT, INSERT, UPDATE ON member TO gym_reception;
-GRANT SELECT, INSERT ON payments TO gym_reception;
-GRANT SELECT ON membership TO gym_reception;
-GRANT SELECT ON v_active_members TO gym_reception;
-GRANT EXECUTE ON pkg_gym_management TO gym_reception;
+CREATE ROLE gym_reception_role;
+CREATE ROLE gym_manager_role;
+CREATE ROLE gym_admin_role;
+
+
+GRANT CREATE SESSION TO admin, gym_reception, gym_manager;
+GRANT UNLIMITED TABLESPACE TO admin;
+
+GRANT SELECT, INSERT, UPDATE ON member TO gym_reception_role;
+GRANT SELECT, INSERT ON payments TO gym_reception_role;
+GRANT SELECT ON membership TO gym_reception_role;
+GRANT SELECT ON v_active_members TO gym_reception_role;
+ GRANT gym_reception_role to gym_reception;
