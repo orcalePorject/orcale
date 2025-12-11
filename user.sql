6b45f8e9776d1190808b96a4530c3d9e9d816496
hamedi786 <154887900+hamedi786@users.noreply.github.com>
2025-12-11 16:47:26 +0430

    privilges to admin user

--- user.sql
+++ user.sql
@@ -15,3 +15,17 @@ GRANT SELECT, INSERT ON payments TO gym_reception_role;
 GRANT SELECT ON membership TO gym_reception_role;
 GRANT SELECT ON v_active_members TO gym_reception_role;
  GRANT gym_reception_role to gym_reception;
+
+ GRANT ALL PRIVILEGES ON member TO gym_admin WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON member_subscriptions TO gym_admin WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON membership TO gym_admin WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON staff TO gym_admin WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON trainers TO gym_admin WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON payments TO gym_admin WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON classes TO gym_admin WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON class_bookings TO gym_admin WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON equipment TO gym_admin WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON lockers TO gym_admin WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON member_attendance TO gym_admin;
+GRANT ALL PRIVILEGES ON staff_attendance TO gym_admin;
+GRANT ALL PRIVILEGES ON trainer_attendance TO gym_admin;
