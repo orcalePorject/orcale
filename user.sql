d23ec11506e338a60a6cb666efebabcefe43c744
hamedi786 <154887900+hamedi786@users.noreply.github.com>
2025-12-11 16:29:25 +0430

    create admin user

--- user.sql
+++ user.sql
@@ -1,6 +1,9 @@
 CREATE USER gym_reception IDENTIFIED BY reception123;
 CREATE USER gym_manager IDENTIFIED BY manager123;
+CREATE USER admin IDENTIFIED BY admin123;
 
+GRANT CREATE SESSION TO gym_admin, gym_reception, gym_manager;
+GRANT UNLIMITED TABLESPACE TO gym_admin;
 GRANT SELECT, INSERT, UPDATE ON member TO gym_reception;
 GRANT SELECT, INSERT ON payments TO gym_reception;
 GRANT SELECT ON membership TO gym_reception;
