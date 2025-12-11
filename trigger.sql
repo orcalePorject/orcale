6d46375a5f5c2ec45890f03586711f560b50ccb0
hamedi786 <154887900+hamedi786@users.noreply.github.com>
2025-12-11 15:10:02 +0430

    create trigger for prevent duplicate_booking

--- trigger.sql
+++ trigger.sql
@@ -1,22 +1,22 @@
-CREATE OR REPLACE TRIGGER trg_update_member_status
-AFTER INSERT OR UPDATE ON member_subscriptions
+<<<<<<< HEAD
+CREATE OR REPLACE TRIGGER trg_prevent_duplicate_booking
+BEFORE INSERT ON class_bookings
 FOR EACH ROW
+DECLARE
+    v_existing_booking NUMBER;
 BEGIN
-    IF :NEW.end_date < SYSDATE THEN
-        DECLARE
-            v_active_sub_count NUMBER;
-        BEGIN
-            SELECT COUNT(*) INTO v_active_sub_count
-            FROM member_subscriptions
-            WHERE m_id = :NEW.m_id
-            AND end_date >= SYSDATE;
-            
-            IF v_active_sub_count = 0 THEN
-                UPDATE member
-                SET status = 'INACTIVE'
-                WHERE m_id = :NEW.m_id;
-            END IF;
-        END;
+    SELECT COUNT(*) INTO v_existing_booking
+    FROM class_bookings
+    WHERE member_id = :NEW.member_id
+    AND class_id = :NEW.class_id
+    AND booking_date = :NEW.booking_date
+    AND status IN ('BOOKED', 'ATTENDED');
+    
+    IF v_existing_booking > 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 
+            'Member already has a booking for this class on selected date');
     END IF;
 END;
 /
+=======
+>>>>>>> 81cfb4cb27fcef74a1fda0e819ec4569ddc86922
